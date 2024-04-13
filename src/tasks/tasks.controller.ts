cadc2fc98de434477156136126e9e29ee01b7ce3
Fernando Balieiro Ferreira <fernando.balieiro@outlook.com>
2024-04-13 17:42:02 -0300

    adicionar getAllTasks para o projeto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,15 +6,22 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './tasks.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTastStatusDto } from './updateTastStatus.dto';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private service: TasksService) {}
+
+  @Get()
+  async getTasks(@Query() filterDto: GetTaskFilterDto): Promise<Task[]> {
+    return await this.service.getTasks(filterDto);
+  }
 
   @Get('/:id')
   getTaskById(@Param('id') id: string): Promise<Task> {
@@ -37,7 +44,7 @@ export class TasksController {
     id: string,
     @Body()
     updateTaskStatusDto: UpdateTastStatusDto,
-  ) {
+  ): Promise<Task> {
     const { status } = updateTaskStatusDto;
     return await this.service.updateStatus(id, status);
   }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { TaskRepository } from './task.repository';
-import { Task } from './tasks.entity';
-import { CreateTaskDto } from './dto/create-task.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { TASK_STATUS } from './task.model';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { TaskRepository } from "./task.repository";
+import { Task } from "./tasks.entity";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { TASK_STATUS } from "./task.model";
+import { GetTaskFilterDto } from "./dto/get-task-filter.dto";
 
 @Injectable()
 export class TasksService {
@@ -11,6 +12,21 @@ export class TasksService {
     @InjectRepository(Task)
     private readonly taskRepository: TaskRepository,
   ) {}
+
+  async getTasks(filterDto: GetTaskFilterDto): Promise<Task[]> {
+    const { status, search } = filterDto;
+    const query = this.taskRepository.createQueryBuilder('task');
+
+    if (status) query.andWhere('task.status = :status', { status });
+
+    if (search)
+      query.andWhere(
+        'task.title LIKE :search OR task.description LIKE :search',
+        { search: `%${search}%` },
+      );
+
+    return await query.getMany();
+  }
 
   async getTaskById(id: string): Promise<Task> {
     const found = await this.taskRepository.findOne({ where: { id: id } });

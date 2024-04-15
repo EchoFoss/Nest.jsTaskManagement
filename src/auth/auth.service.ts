9be8d6ffac71b6872392173fcac0ac9cfc6b0451
Fernando Balieiro Ferreira <fernando.balieiro@outlook.com>
2024-04-15 00:45:45 -0300

    feat: adiciona hashing para a senha do usuário
    Anteriormente, as senhas eram guardadas como plain text, a partir de
    agora, elas passarão por um processo de hashing antes de serem guardadas
    no banco de dados

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UsersRepository } from './users.repository';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -13,7 +18,13 @@ export class AuthService {
   // Sign up
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.repository.create({ username, password });
+    // hash
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.repository.create({
+      username,
+      password: hashedPassword,
+    });
     try {
       await this.repository.save(user);
     } catch (error) {
